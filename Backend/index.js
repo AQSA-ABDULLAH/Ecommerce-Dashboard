7b117a9e0e3a47c7ecd728ce529fe7a68760c19e
AQSA-ABDULLAH <aqsaabdullah38403@gmail.com>
2023-08-30 13:05:01 +0500

    create login Api

--- Backend/index.js
+++ Backend/index.js
@@ -12,7 +12,20 @@ app.use(cors());
 app.post("/register", async(req, res)=>{
     let user = new Users(req.body);
     let result = await user.save();
+    result = result.toObject();
+    delete result.password;
     res.send(result);
+});
+
+app.post("/login", async(req, res)=>{
+    if(req.body.password && req.body.email){
+        let user = await Users.findOne(req.body).select("-password");
+        if(user){
+            res.send(user);
+        }else{
+            res.send({result:"Not Found"});
+        }
+    }
 });
 
 app.listen(4000);
