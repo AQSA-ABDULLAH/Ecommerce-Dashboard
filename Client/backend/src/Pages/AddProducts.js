6ffb80a7d5e5c9d6b1053d3bef85babfdc9165bc
AQSA-ABDULLAH <aqsaabdullah38403@gmail.com>
2023-08-31 18:02:03 +0500

    update add product component

--- Client/backend/src/Pages/AddProducts.js
+++ Client/backend/src/Pages/AddProducts.js
@@ -1,35 +1,69 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate} from 'react-router-dom';
 import './Style.css';
 import Sidebar from '../Components/Sidebar';
 import Navbar from '../Components/Navbar';
+
 export default function AddProducts() {
+  const [title, setTitle] = useState("");
+  const [des, setDes] = useState("");
+  const [price, setPrice] = useState("");
+  const [image, setImage]=useState();
+  const navigate = useNavigate();
+
+  const uploadImage = (e) =>{
+    console.log(e.target.file);
+    setImage(e.target.file);
+  }
+
+  const collectData = async (e) => {
+    e.preventDefault();
+    let result = await fetch('http://localhost:4000/add_product', {
+      method: 'post',
+      body: JSON.stringify({ title, des, price }),
+      headers: {
+        'Content-Type': 'application/json'
+      },
+    });
+    result = await result.json();
+    // localStorage.setItem("product", JSON.stringify(result));
+    console.log(result)
+    navigate('/');
+  }
   return (
     <div>
-      <div class="row">
-        <div class="col-2" >
+      <div className="row">
+        <div className="col-2" >
           <Sidebar />
         </div>
-        <div class="col-10" >
+        <div className="col-10" >
           <Navbar />
           <div className='container'>
-            <form >
+            <form onSubmit={collectData}>
               <h2 className='text-center pt-3'>ADD NEW PRODUCT</h2>
+              <div className="mb-3">
+                <input type="file" accept='image/' className="form-control"
+                onChange={uploadImage}/>
+              </div>
               <div className="mb-3">
                 <label className="form-label">Title</label>
                 <input type="text" className="form-control" id="exampleInputname"
-                />
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}/>
               </div>
               <div className="mb-3">
                 <label className="form-label">Description</label>
                 <input type="email" className="form-control" id="exampleInputEmail1"
-                />
+                value={des}
+                onChange={(e) => setDes(e.target.value)}/>
               </div>
               <div className="mb-3">
                 <label className="form-label">Price</label>
-                <input type="password" className="form-control" id="exampleInputPassword1"
-                />
+                <input type="int" className="form-control" id="exampleInputPassword1"
+                value={price}
+                onChange={(e) => setPrice(e.target.value)}/>
               </div>
-              <button type="submit" className="btn btn-success">Save</button>
+              <button type="submit" className="btn btn-success">ADD PRODUCT</button>
             </form>
           </div>
         </div>
--- Server/Models/Product.js
+++ Server/Models/Product.js
@@ -4,7 +4,5 @@ const productSchema = new mongoose.Schema({
     title:String,
     description:String,
     price:String,
-    quantity:Number,
-    options:String
 });
 module.exports = mongoose.model("products",productSchema);
