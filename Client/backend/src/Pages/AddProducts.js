54b98860bd0a4078db88bcc9c5252101dd5bb5dd
AQSA-ABDULLAH <aqsaabdullah38403@gmail.com>
2023-08-31 17:24:39 +0500

    create add product component

--- Client/backend/src/Pages/AddProducts.js
+++ Client/backend/src/Pages/AddProducts.js
@@ -10,9 +10,27 @@ export default function AddProducts() {
           <Sidebar />
         </div>
         <div class="col-10" >
-          <Navbar/>
-          <div>
-            Add products
+          <Navbar />
+          <div className='container'>
+            <form >
+              <h2 className='text-center pt-3'>ADD NEW PRODUCT</h2>
+              <div className="mb-3">
+                <label className="form-label">Title</label>
+                <input type="text" className="form-control" id="exampleInputname"
+                />
+              </div>
+              <div className="mb-3">
+                <label className="form-label">Description</label>
+                <input type="email" className="form-control" id="exampleInputEmail1"
+                />
+              </div>
+              <div className="mb-3">
+                <label className="form-label">Price</label>
+                <input type="password" className="form-control" id="exampleInputPassword1"
+                />
+              </div>
+              <button type="submit" className="btn btn-success">Save</button>
+            </form>
           </div>
         </div>
       </div>
