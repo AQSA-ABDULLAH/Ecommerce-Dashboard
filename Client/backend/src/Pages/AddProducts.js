d281cca73f3265e7cede12e69778a0d43cc2df4d
AQSA-ABDULLAH <aqsaabdullah38403@gmail.com>
2023-09-06 12:25:15 +0500

    form validation of add product

--- Client/backend/src/Pages/AddProducts.js
+++ Client/backend/src/Pages/AddProducts.js
@@ -11,29 +11,39 @@ export default function AddProducts() {
   const [category, setCategory] = useState("");
   const [price, setPrice] = useState("");
   const [image, setImage] = useState();
+  const [error, setError] = useState(false);
   const navigate = useNavigate();
 
   const handleClick = () => {
+    if (!image || !title || !description || !category || !price) {
+      setError(true);
+      return false;
+    }
 
-    const formData = new FormData();
-    formData.append('image', image)
-    formData.append('title', title)
-    formData.append('description', description)
-    formData.append('category', category)
-    formData.append('price', price)
-    
-    axios.post('http://localhost:4000/add_product', formData,
-      {
-        headers: {"Content-Type": "multipart/form-data"},
-      }
-    )
-      .then((res) => {
-        console.log(res.data)
-      })
-      .catch(err => {
-        console.log(err, "err")
-      })
-    navigate('/');
+    if (isNaN(price)) {
+      setError(true);
+      return false;
+    }
+  
+      const formData = new FormData();
+      formData.append('image', image)
+      formData.append('title', title)
+      formData.append('description', description)
+      formData.append('category', category)
+      formData.append('price', price)
+
+      axios.post('http://localhost:4000/add_product', formData,
+        {
+          headers: { "Content-Type": "multipart/form-data" },
+        }
+      )
+        .then((res) => {
+          console.log(res.data)
+        })
+        .catch(err => {
+          console.log(err, "err")
+        })
+      navigate('/');
   }
 
   return (
@@ -47,38 +57,70 @@ export default function AddProducts() {
           <div className='container'>
             <form>
               <h2 className='text-center pt-3'>ADD NEW PRODUCT</h2>
+
               <div className="mb-3">
-                <input type="file" className="form-control"
-                  onChange={(e) => setImage(e.target.files[0])} />
+                <input
+                  type="file"
+                  className="form-control"
+                  onChange={(e) => setImage(e.target.files[0])}
+                />
+                {error && !image && <span className="text-danger">Plz Select Any Image</span>}
               </div>
+
               <div className="mb-3">
                 <label className="form-label">Title</label>
-                <input type="text" className="form-control" id="exampleInputname"
+                <input
+                  type="text"
+                  className="form-control"
+                  id="exampleInputname"
                   value={title}
-                  onChange={(e) => setTitle(e.target.value)} />
+                  onChange={(e) => setTitle(e.target.value)}
+                />
+                {error && !title && <span className="text-danger">Enter Valid Title</span>}
               </div>
+
               <div className="mb-3">
                 <label className="form-label">Description</label>
-                <input type="text" className="form-control" id="exampleInputDes"
+                <input
+                  type="text"
+                  className="form-control"
+                  id="exampleInputDes"
                   value={description}
-                  onChange={(e) => setDescription(e.target.value)} />
+                  onChange={(e) => setDescription(e.target.value)}
+                />
+                {error && !description && <span className="text-danger">Enter Valid Description</span>}
               </div>
+
               <div className="mb-3">
                 <label className="form-label">Category</label>
-                <input type="text" className="form-control" id="exampleInputCat"
+                <input
+                  type="text"
+                  className="form-control"
+                  id="exampleInputCat"
                   value={category}
-                  onChange={(e) => setCategory(e.target.value)} />
+                  onChange={(e) => setCategory(e.target.value)}
+                />
+                {error && !category && <span className="text-danger">Select Category of food</span>}
               </div>
+
               <div className="mb-3">
                 <label className="form-label">Price</label>
-                <input type="int" className="form-control" id="exampleInputPassword1"
+                <input
+                  type="int"
+                  className="form-control"
+                  id="exampleInputPassword1"
                   value={price}
-                  onChange={(e) => setPrice(e.target.value)} />
+                  onChange={(e) => setPrice(e.target.value)}
+                />
+                {error && !price && <span className="text-danger">Enter Valid Price of Item</span>}
+                {error && price && isNaN(price) && <span className="text-danger">Price must be a number</span>}
               </div>
-              <button type="submit" className="btn btn-success"
-                onClick={handleClick}>
-                ADD PRODUCT</button>
+
+              <button type="button" className="btn btn-success" onClick={handleClick}>
+                ADD PRODUCT
+              </button>
             </form>
+
           </div>
         </div>
       </div>
