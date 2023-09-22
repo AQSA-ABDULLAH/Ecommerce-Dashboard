5dbcd276ab4d1c252f24a2b1ec323fb574e943e5
AQSA-ABDULLAH <aqsaabdullah38403@gmail.com>
2023-09-22 23:58:07 +0500

    display product data

--- Client/frontend/src/Components/Card.js
+++ Client/frontend/src/Components/Card.js
@@ -1,32 +1,45 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 export default function Card() {
+    const [product, setProduct] = useState([]);
+    useEffect(() => {
+        getProducts();
+    }, [])
+
+    const getProducts = async () => {
+        let result = await fetch("http://localhost:4000/");
+        result = await result.json();
+        setProduct(result);
+    }
     return (
         <div>
             <div>
-                <div className="card mt-3" style={{ "width": "18rem", "maxHeight": "360px" }}>
-                    <img src='https://mdbcdn.b-cdn.net/img/new/slides/041.webp' className='card-img-top' alt=''/>
-                    <div className="card-body">
-                        <h5 className="card-title">Card title</h5>
-                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <div classNameName='container w-100'>
-                            <select className='m-2 h-100 bg-success rounded'>
-                                {Array.from(Array(6), (e, i) => {
-                                    return (
-                                        <option key={i + 1} value={i + 1}> {i + 1} </option>
-                                    )
-                                })}
-                            </select>
-                            <select className='m-2 h-100 bg-success rounded'>
-                                <option value="half">Half</option>
-                                <option value="full">Full</option>
-                            </select>
-                            <div className='d-inline h-100 fs-5'>
-                                Total Price
+                {
+                    product.map((item, index) =>
+                    <div className="card mt-3" style={{ "width": "18rem", "maxHeight": "360px" }}>
+                        <img src='https://mdbcdn.b-cdn.net/img/new/slides/041.webp' className='card-img-top' alt='' />
+                        <div className="card-body">
+                            <h5 className="card-title">{item.title}</h5>
+                            <p className="card-text">{item.description}</p>
+                            <div classNameName='container w-100'>
+                                <select className='m-2 h-100 bg-success rounded'>
+                                    {Array.from(Array(6), (e, i) => {
+                                        return (
+                                            <option key={i + 1} value={i + 1}> {i + 1} </option>
+                                        )
+                                    })}
+                                </select>
+                                <select className='m-2 h-100 bg-success rounded'>
+                                    <option value="half">Half</option>
+                                    <option value="full">Full</option>
+                                </select>
+                                <div className='d-inline h-100 fs-5'>
+                                    Total Price {item.price}
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
+                )}
             </div>
         </div>
     )
